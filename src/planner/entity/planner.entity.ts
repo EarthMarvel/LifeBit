1c7fb5c2b3d1cacaf0e2e338ea6ff963c1d6a859
우성종 <sjwoo1999@korea.ac.kr>
2024-04-05 17:43:19 +0900

    🔨 Modified: Merging PR on feature/mission #2
    Modified: Merging PR on feature/mission #2

--- src/planner/entity/planner.entity.ts
+++ src/planner/entity/planner.entity.ts
@@ -1,29 +1,37 @@
-import { Column, CreateDateColumn, Entity, Index, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Plan } from "./plan.entity";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Plan } from './plan.entity';
+import { User } from 'src/user/entities/user.entity';
 
-@Entity({name : 'planner'})
+@Entity({ name: 'planner' })
 export class Planner {
-    
-    @PrimaryGeneratedColumn()
-    plannerId : number;
+  @PrimaryGeneratedColumn()
+  plannerId: number;
 
-    @OneToMany(() => Plan, (plan) => plan.planner)
-    plan : Plan[]
+  @OneToMany(() => Plan, (plan) => plan.planner)
+  plan: Plan[];
 
-    //임의 userId
-    @Column({type : 'varchar', unique : true, nullable : false})
-    userId : number;
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
 
-    @Column({type : 'varchar'})
-    name? : string;
+  @Column({ type: 'varchar', nullable: true })
+  name: string;
 
-    @Column({type : 'varchar'})
-    description? : string;
-  
-    @CreateDateColumn() 
-    createdAt: Date;
-    
-    @UpdateDateColumn() 
-    updatedAt: Date;
+  @Column({ type: 'varchar', nullable: true })
+  description: string;
 
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
--- src/planner/planner.controller.ts
+++ src/planner/planner.controller.ts
@@ -1,130 +1,172 @@
-import { Body, Controller, Delete, Get, HttpStatus, Post, Put, Query, Res } from '@nestjs/common';
-import { PlannerService } from './planner.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Post,
+  Put,
+  Query,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+
 import { Response } from 'express';
 import { TodoDto } from './dto/todo.dto';
 import { DateDto } from './dto/get.planner.dto';
 import { PlannerDto } from './dto/update.planner.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { UserInfo } from 'src/user/decorator/userInfo.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { PlannerService } from './planner.service';
 
 @Controller('myPage')
+@UseGuards(AuthGuard('jwt'))
 export class PlannerController {
-    constructor(private readonly plannerService : PlannerService) {}
+  constructor(private readonly plannerService: PlannerService) {}
 
-    /**
-     * 처음 마이페이지 접근할 때 호출될 API
-     * 이번 달 계획 / 오늘 계획 (오늘 계획은 시간 데이터까지 필요하다)
-     * user 로직과 통합 시 -> userinfo 에서 user 조회 예정
-     */
-    @Get('/')
-    async myPage(@Res() res : Response) {
-        const userId = 1; //가드 필요
+  /**
+   * 마이페이지 호출
+   * 이번 달 계획 / 오늘 계획 (오늘 계획은 시간 데이터까지 필요하다) -> DB I/O 많음, 리팩토링 필요
+   * user 로직과 통합 시 -> userinfo 에서 user 조회 예정
+   * @param res
+   * @returns
+   */
+  @Get('/')
+  async myPage(@UserInfo() user: User, @Res() res: Response) {
+    console.log('user : ' + user.user_id); //1
+    return res.status(HttpStatus.OK).json({
+      message: '마이 페이지를 조회했습니다.',
+      data: await this.plannerService.myPage(user),
+    });
+  }
 
-        return res.status(HttpStatus.OK).json({
-        message : "마이 페이지를 조회했습니다.",
-        data : await this.plannerService.myPage(userId)
-        })
-    }
+  /**
+   * 플래너 조회
+   * @param plannerId
+   * @param user
+   * @param dateDto
+   * @param res
+   * @returns
+   */
+  @Get('/planner')
+  async getPlanner(
+    @Query('plannerId') plannerId: number,
+    @Body() dateDto: DateDto,
+    @Res() res: Response,
+  ) {
+    return res.status(HttpStatus.OK).json({
+      message: '플래너를 조회했습니다.',
+      data: await this.plannerService.getPlanner(dateDto, plannerId),
+    });
+  }
 
-    /**
-     * 플래너 조회
-     */
-    @Get('/planner')
-    async getPlanner(@Body() dateDto : DateDto, @Res() res : Response) {
-        const plannerId = 1;
+  /**
+   * 플래너 수정
+   * @param plannerDto
+   * @param res
+   * @returns
+   */
+  @Put('/planner')
+  async updatePlanner(
+    @Query('plannerId') plannerId: number,
+    @Body() plannerDto: PlannerDto,
+    @Res() res: Response,
+  ) {
+    return res.status(HttpStatus.OK).json({
+      message: '플래너가 수정되었습니다.',
+      data: await this.plannerService.updatePlanner(plannerDto, plannerId),
+    });
+  }
 
-        return res.status(HttpStatus.OK).json({
-        message : "플래너를 조회했습니다.",
-        data : await this.plannerService.getPlanner(dateDto, plannerId)
-        })
-    }
+  /**
+   * 일정 등록
+   * @param todoDto
+   * @param res
+   * @returns
+   */
+  @Post('/todo')
+  async postTodo(
+    @Query('plannerId') plannerId: number,
+    @Body() todoDto: TodoDto,
+    @Res() res: Response,
+  ) {
+    return res.status(HttpStatus.CREATED).json({
+      message: '일정을 등록했습니다.',
+      data: await this.plannerService.postTodo(todoDto, plannerId),
+    });
+  }
 
-    /**
-     * 플래너 수정 (여기서부터 작업)
-     */
-    @Put('/planner')
-    async updatePlanner(@Body() plannerDto : PlannerDto, @Res() res : Response) {
-        const plannerId = 1;
+  /**
+   * 일정 수정
+   * @param todoDto
+   * @param res
+   * @returns
+   */
+  @Put('/todo')
+  async updateTodo(
+    @Query('planId') planId: number,
+    @Body() todoDto: TodoDto,
+    @Res() res: Response,
+  ) {
+    return res.status(HttpStatus.OK).json({
+      message: '일정이 수정되었습니다.',
+      data: await this.plannerService.updateTodo(planId, todoDto),
+    });
+  }
 
-        return res.status(HttpStatus.OK).json({
-        message : "플래너가 수정되었습니다.",
-        data : await this.plannerService.updatePlanner(plannerDto, plannerId)
-        })
-    }
+  /**
+   * 일정 삭제
+   * @param res
+   * @returns
+   */
+  @Delete('/todo')
+  async deleteTodo(@Query('planId') planId: number, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json({
+      message: '일정이 삭제되었습니다.',
+      data: await this.plannerService.deleteTodo(planId),
+    });
+  }
 
-        
-    /**
-     * 일정 등록
-     */
-    @Post('/todo')
-    async postTodo(@Body() todoDto : TodoDto, @Res() res : Response) {
-        const planId = 1;
+  /**
+   * 일정 체크, 언체크
+   * @param res
+   * @returns
+   */
+  @Post('/todo/check')
+  async checkTodo(@Query('planId') planId: number, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json({
+      message: '일정이 체크 / 언체크 되었습니다.',
+      data: await this.plannerService.checkTodo(planId),
+    });
+  }
 
-        return res.status(HttpStatus.CREATED).json({
-        message : "일정을 등록했습니다.",
-        data : await this.plannerService.postTodo(todoDto, planId)
-        })
-    }
+  /**
+   * 일정 인증, 추후 인증 API 추가, 추후 포인트 반영
+   * @param res
+   * @returns
+   */
+  @Post('/todo/auth')
+  async authTodo(
+    @UserInfo() user: User,
+    @Query('planId') planId: number,
+    @Res() res: Response,
+  ) {
+    return res.status(HttpStatus.OK).json({
+      message: '일정이 인증되었습니다.',
+      data: await this.plannerService.authTodo(planId),
+    });
+  }
 
-    /**
-     * 일정 수정
-     */
-    @Put('/todo')
-    async updateTodo(@Body() todoDto : TodoDto, @Res() res : Response) {
-        const planId = 1;
+  /**
+   * 내 미션 확인하기
+   */
 
-        return res.status(HttpStatus.OK).json({
-        message : "일정이 수정되었습니다.",
-        data : await this.plannerService.updateTodo(planId, todoDto)
-        })
-    }
+  /**
+   * 프로필 사진 수정하기
+   */
 
-    /**
-     * 일정 삭제
-     */
-    @Delete('/todo')
-    async deleteTodo(@Res() res : Response) {
-        const planId = 1;
-
-        return res.status(HttpStatus.OK).json({
-        message : "일정이 삭제되었습니다.",
-        data : await this.plannerService.deleteTodo(planId)
-        })
-    }
-
-    /**
-     * 일정 체크 / 언체크
-     */
-    @Post('/todo/check')
-    async checkTodo(@Res() res : Response) {
-        const planId = 2;
-
-        return res.status(HttpStatus.OK).json({
-        message : "일정이 체크 / 언체크 되었습니다.",
-        data : await this.plannerService.checkTodo(planId)
-        })
-    }
-
-    /**
-     * 일정 인증
-     * 추후 인증 API 추가
-     * 추후 포인트 반영
-     */
-    @Post('/todo/auth')
-    async authTodo(@Res() res : Response) {
-        const planId = 2;
-
-        return res.status(HttpStatus.OK).json({
-        message : "일정이 인증되었습니다.",
-        data : await this.plannerService.authTodo(planId)
-        })
-    }
-
-    /**
-     * 내 미션 확인하기 //미션 entity 올라오면 수정하기
-     */
-
-
-    /**
-     * 프로필 사진 수정하기 //회원에서 s3 올리면 수정하기
-     */
-
+  /**
+   * 프로필 사진 수정하기 //회원에서 s3 올리면 수정하기
+   */
 }
