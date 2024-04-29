899b90a9c8db7f81cca57cbb7b75836c949c6f28
Kimminseok <alsehf0000@naver.com>
2024-04-29 20:21:55 +0900

    Feat: 좋아요 버튼

--- src/socket/likes.gateway.ts
+++ src/socket/likes.gateway.ts
@@ -1,13 +1,31 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
 @WebSocketGateway()
 export class LikeGateway {
   @WebSocketServer() server: Server;
 
+  @SubscribeMessage('like')
   LikeNotification(boardId: number, userId: number) {
-    console.log('알림 찍히는거 보쏘');
-    this.server.emit('likeNotification', { boardId, userId });
+    console.log('좋아요 알림');
+    this.server.emit(
+      'like',
+      { boardId, userId },
+      { message: '좋아요 알림을 받았습니다!' },
+    );
   }
 }
-// 알림  프론트에서 띄우기
+
+// like.gateway.ts
+
+//   @SubscribeMessage('like')
+//   handleLikeEvent(@MessageBody() data: any): void {
+//     console.log(`좋아요 알림: ${data.message}`);
+//     // 모든 클라이언트에게 'like' 이벤트를 발생시킴
+//     this.server.emit('like', data);
+//   }
+// }
